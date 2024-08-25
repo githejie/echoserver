74e699d7a893fed6a809dd698212a20c4f417012
githejie <www.617311008@qq.com>
2024-08-25 15:15:28 +0800

    Add functional tests

---
+++ script/test_echo_server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+IP="127.0.0.1"
+PORT="2233"
+MESSAGE="Hello, Echo Server!"
+
+./build/echoserver $IP $PORT &
+sleep 1
+
+echo "Sending message to echo server: $MESSAGE"
+RESPONSE=$(echo $MESSAGE | nc -w 2 $IP $PORT)
+
+if [ "$RESPONSE" == "$MESSAGE" ]; then
+    echo "Echo server is working correctly! Response: $RESPONSE"
+else
+    echo "Unexpected response from echo server: $RESPONSE"
+    exit 1
+fi
